88b547f0548483eab5bed275f373a5f1d15c2cea
Geniuskaa <eminizinar@gmail.com>
2021-02-06 21:23:03 +0300

    Initial commit

---
+++ cmd/client/client.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	Extract()
+}
+
+type XmlData struct {
+	ID       string  `xml:"ID,attr"`
+	NumCode  int64   `xml:"NumCode"`
+	CharCode string  `xml:"CharCode"`
+	Nominal  int64   `xml:"Nominal"`
+	Name     string  `xml:"Name"`
+	Value    float64 `xml:"Value"`
+}
+
+type Curriencies struct {
+	XMLName   xml.Name  `xml:"ValCurs"`
+	Date      string    `xml:"Date,attr"`
+	Name      string    `xml:"name,attr"`
+	ValuteIds []XmlData `xml:"Valute"`
+}
+
+type JsonData struct {
+	Code string `json:"code"`
+	Name string `json:"name"`
+	Value float64 `json:"value"`
+}
+
+
+
+func Extract() error { // Скачивает файл, его переделываем в json и сохраняем как currencies.json
+	reqUrl := "https://raw.githubusercontent.com/netology-code/bgo-homeworks/master/10_client/assets/daily.xml"
+	resp, err := http.Get(reqUrl)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	log.Println(resp.StatusCode)
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	defer func() {
+		if cerr := resp.Body.Close(); cerr != nil {
+			log.Println(cerr)
+		}
+	}()
+
+	parsedData, err := parseXml(body)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	jsonData := parsedData.convertDataToJson()
+
+	err = writeDataToJsonFile(jsonData)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+
+func parseXml(data []byte) (Curriencies, error) {
+	var decoded Curriencies
+
+	err := xml.Unmarshal(data, &decoded)
+	if err != nil {
+		log.Println(err)
+		return Curriencies{
+			ValuteIds: nil,
+		}, err
+	}
+
+	return decoded, nil
+}
+
+func (c *Curriencies) convertDataToJson() []JsonData {
+	var jsonData []JsonData
+	for _, element := range c.ValuteIds{
+		jsonData = append(jsonData, JsonData{
+			Code:  element.CharCode,
+			Name:  element.Name,
+			Value: element.Value,
+		})
+	}
+
+	return jsonData
+}
+
+func writeDataToJsonFile(data []JsonData) error {
+	file, err := os.Create("currencies.json")
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	defer func(c io.Closer) {
+		if err := c.Close(); err != nil {
+			log.Println(err)
+		}
+	}(file)
+
+
+	err = json.NewEncoder(file).Encode(data)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Geniuskaa/Task9.1_BGO-3
+
+go 1.15
